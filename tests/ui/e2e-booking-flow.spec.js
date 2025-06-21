aa166fab1c272c28c86c64ebb89a7c077e20a956
Ravi Pandey <42024320+rpravi1404@users.noreply.github.com>
2025-06-21 19:30:15 +0530

    Update e2e-booking-flow.spec.js

--- tests/ui/e2e-booking-flow.spec.js
+++ tests/ui/e2e-booking-flow.spec.js
@@ -1,7 +1,16 @@
 import { test, expect } from '@playwright/test';
+import { 
+  selectRoom, 
+  fillBookingForm, 
+  checkAvailability,
+  getBookingConfirmation
+} from '../../utils/uiHelpers.js';
 
 test('Booking flow', async ({ page }) => {
   await page.goto('https://automationintesting.online/');
-  await page.waitForTimeout(5000);
-  
+  await checkAvailability(page);
+  await selectRoom(page, 'Single');
+  await fillBookingForm(page);
+  const bookingConfirmation = await getBookingConfirmation(page);
+  expect(bookingConfirmation).toEqual('Booking Confirmed');
 });
